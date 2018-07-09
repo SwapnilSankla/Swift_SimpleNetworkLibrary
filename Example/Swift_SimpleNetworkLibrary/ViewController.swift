7f376c1a99407b171103db7cfa489897834c630e
Swapnil Sankla <swapnil.sankla@gmail.com>
2018-07-09 14:40:11 +0530

    Support for simple GET request

--- Example/Swift_SimpleNetworkLibrary/ViewController.swift
+++ Example/Swift_SimpleNetworkLibrary/ViewController.swift
@@ -1,24 +1,34 @@
-//
-//  ViewController.swift
-//  Swift_SimpleNetworkLibrary
-//
-//  Created by SwapnilSankla on 07/09/2018.
-//  Copyright (c) 2018 SwapnilSankla. All rights reserved.
-//
-
 import UIKit
+import Swift_SimpleNetworkLibrary
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let successHandler: ([Employee]) -> Void = { (employees) in
+            print(employees)
+        }
+        let errorHandler: (String) -> Void = { (error) in
+            print(error)
+        }
+
+        if let url = URL(string: "http://dummy.restapiexample.com/api/v1/employees") {
+            NetworkLayer().get(url: url,
+                               successHandler: successHandler,
+                               errorHandler: errorHandler)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+struct Employee: Codable {
+    let employee_name: String
+    let employee_salary: String
+    let employee_age: String
 }
 
---
+++ Swift_SimpleNetworkLibrary/Classes/NetworkLayer.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+typealias NetworkCompletionHandler = (Data?, URLResponse?, Error?) -> Void
+
+public class NetworkLayer {
+    static let genericError = "Something went wrong. Please try again later"
+
+    public init() {
+    }
+
+    public func get<T: Decodable>(url: URL,
+                           successHandler: @escaping (T) -> Void,
+                           errorHandler: @escaping (String) -> Void) {
+
+        let completionHandler: NetworkCompletionHandler = { (data, urlResponse, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                errorHandler(NetworkLayer.genericError)
+                return
+            }
+
+            if let unwrappedUrlResponse = urlResponse as? HTTPURLResponse {
+                if self.isSuccessCode(unwrappedUrlResponse.statusCode) {
+                    if let data = data {
+                        if let responseObject = try? JSONDecoder().decode(T.self, from: data) {
+                            successHandler(responseObject)
+                            return
+                        } else {
+                            print("Unable to parse the response in given type \(T.self)")
+                        }
+                    }
+                }
+            }
+
+            errorHandler(NetworkLayer.genericError)
+        }
+
+        URLSession.shared.dataTask(with: url, completionHandler: completionHandler).resume()
+    }
+
+    private func isSuccessCode(_ statusCode: Int) -> Bool {
+        return statusCode >= 200 && statusCode < 300
+    }
+}
